b5a6741a4f29cc7e7c5928221b917dc9a58e1b26
victordu45 <victor.charderon@hotmail.fr>
2019-11-08 16:24:05 -0500

    Ajout requete httplogin et sign in + interface sign in

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,25 +1,27 @@
 import { Platform } from '@ionic/angular';
-import { Injectable } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
  
 const TOKEN_KEY = 'auth-token';
 const TEST_LOGIN = 'usertest';
 const TEST_PASSWORD = 'test';
- 
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthenticationService {
  
   name : '';
   password:'';
 
-  verifyIfLogged =false;
+  verifyIfLogged =true;
   
   authenticationState = new BehaviorSubject(false);
  
-  constructor(private storage: Storage, private plt: Platform) { 
+  constructor(private storage: Storage, private plt: Platform, private http: HttpClient) { 
     this.plt.ready().then(() => {
       this.checkToken();
     });
@@ -45,23 +47,51 @@ export class AuthenticationService {
     return this.verifyIfLogged;
   }
 
+  requeteLogin(pUser:string,pPassword:string){
+    let json = {
+      user : pUser,
+      password : pPassword
+    }
+    //header
+    let httpoption = {headers : new HttpHeaders({
+      'Content-Type' : 'application/json',
+      'Access-Control-Allow-Origin':'*'
+    })};
+
+
+      this.http.post('http://192.168.0.158:5000/login', json,httpoption).subscribe(
+        data=>{
+          console.log(data);
+          if(data['value'] == true){
+            this.verifyIfLogged=true;
+            this.storage.set(TOKEN_KEY, pUser).then(() => {
+              this.authenticationState.next(true);
+            });
+          }
+          else{
+            this.authenticationState.next(false);
+            this.verifyIfLogged=false;
+          }
+        });
+  }
+
   login(pUser:string, pPassword:string) {
 
     //faire la verification de l'utilisateur
     //recuperationIdentifiants
-    
+    this.requeteLogin(pUser,pPassword);
 
-    
-    if(pUser == TEST_LOGIN && pPassword == TEST_PASSWORD){
-     this.storage.set(TOKEN_KEY, pUser).then(() => {
-        this.authenticationState.next(true);
-      });
-      return true;
-    }
-    else{
-      this.authenticationState.next(false);
-      return false;
-    }
+    // if(this.verifyIfLogged){
+    //  this.storage.set(TOKEN_KEY, pUser).then(() => {
+    //     this.authenticationState.next(true);
+    //   });
+    //   return true;
+    // }
+    // else{
+    //   this.verifyIfLogged=false;
+    //   this.authenticationState.next(false);
+    //   return false;
+    // }
 
   }
  
