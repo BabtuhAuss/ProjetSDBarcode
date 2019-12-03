b418a788a48c1beb6b94f3b0627756032be27834
Baptiste Aussenac <aussenacb@gmail.com>
2019-12-02 23:51:24 -0500

    modification travial page edit produit

--- src/app/members/dashboard/produit/edit-produit/edit-produit.module.ts
+++ src/app/members/dashboard/produit/edit-produit/edit-produit.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { EditProduitPage } from './edit-produit.page';
+import { Camera } from '@ionic-native/camera/ngx';
 
 const routes: Routes = [
   {
@@ -21,6 +22,8 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
+  providers:[Camera]
+  ,
   declarations: [EditProduitPage]
 })
 export class EditProduitPageModule {}
--- src/app/members/dashboard/produit/edit-produit/edit-produit.page.ts
+++ src/app/members/dashboard/produit/edit-produit/edit-produit.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Produit } from 'src/app/Produit';
 import { ActivatedRoute } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-edit-produit',
@@ -10,15 +10,52 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 
 export class EditProduitPage implements OnInit {
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+  customActionSheetOptions: any = {
+    header: 'Nutriscore',
+    subHeader: 'Choisissez le score nutri'
+  };
+  
 
   produitEdit: Produit;
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute , private camera: Camera) {
   }
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.produitEdit = JSON.parse(params["produit"]),
-        console.log("produit dans la page de modification ! " + this.produitEdit);
+      this.produitEdit = JSON.parse(params["produit"]);
     });
   }
+
+  modifNutri($event){
+    this.produitEdit.nutriscore = $event.target.value; 
+  }
+
+  modifNomProduit($event){
+    this.produitEdit.nomProduit = $event.target.value;
+  }
+
+  picturePrincipale(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.produitEdit.urlimage = base64Image;
+     }, (err) => {
+      // Handle error
+     });
+     
+  }
+
+  modifImgPrincipale(val : string){
+
+  }
+
 
 }
