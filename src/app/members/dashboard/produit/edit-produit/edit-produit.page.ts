4b393772fa8d35846ff12f41c9baedc81fd29257
Baptiste Aussenac <aussenacb@gmail.com>
2019-12-05 01:14:05 -0500

    edition correcte d'un produit

--- src/app/members/dashboard/produit/edit-produit/edit-produit.page.ts
+++ src/app/members/dashboard/produit/edit-produit/edit-produit.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Produit } from 'src/app/Produit';
 import { ActivatedRoute } from '@angular/router';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-edit-produit',
@@ -12,7 +14,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 export class EditProduitPage implements OnInit {
   options: CameraOptions = {
     quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
+    destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
   }
@@ -21,10 +23,10 @@ export class EditProduitPage implements OnInit {
     header: 'Nutriscore',
     subHeader: 'Choisissez le score nutri'
   };
-  
+
 
   produitEdit: Produit;
-  constructor(private route: ActivatedRoute , private camera: Camera) {
+  constructor(private route: ActivatedRoute, private camera: Camera, private http: HttpClient) {
   }
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -32,30 +34,91 @@ export class EditProduitPage implements OnInit {
     });
   }
 
-  modifNutri($event){
-    this.produitEdit.nutriscore = $event.target.value; 
+  modifNutri($event) {
+    this.produitEdit.nutriscore = $event.target.value;
   }
 
-  modifNomProduit($event){
+  modifNomProduit($event) {
     this.produitEdit.nomProduit = $event.target.value;
   }
-
-  picturePrincipale(){
+  pictureNutri() {
     this.camera.getPicture(this.options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64 (DATA_URL):
-
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-      this.produitEdit.urlimage = base64Image;
-     }, (err) => {
+      console.log("premier test");
+      this.produitEdit.urlimagenut = 'data:image/jpeg;base64,' + imageData;
+      console.log("test2");
+    }, (err) => {
       // Handle error
-     });
-     
-  }
-
-  modifImgPrincipale(val : string){
+    });
 
   }
+
+  pictureIng() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.produitEdit.urlimageing = 'data:image/jpeg;base64,' + imageData;
+
+    }, (err) => {
+      // Handle error
+    });
+
+  }
+  picturePrincipale() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      console.log("premier test");
+      this.produitEdit.urlimage = 'data:image/jpeg;base64,' + imageData;
+      this.produitEdit.urlimagesmall = 'data:image/jpeg;base64,' + imageData;
+      console.log("test2");
+    }, (err) => {
+      // Handle error
+    });
+
+  }
+
+  delete(val: string) {
+    switch (val) {
+      case 'p':
+        this.produitEdit.urlimage = '';
+        this.produitEdit.urlimagesmall = '';
+        break;
+      case 'n':
+        // code block
+        break;
+      case 'i':
+        // code block
+        break;
+      default:
+      // code block
+    }
+  }
+
+
+
+  updateHttp() {
+    let json = JSON.stringify(this.produitEdit);
+
+    let httpoption = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+    let testProduit
+    var adresseRequest = environment.adressePython + "/updateProduit"
+    this.http.post(adresseRequest, json, httpoption).subscribe(
+      data => {
+        console.log("data" + data);
+        
+      }
+    );
+    return testProduit;
+  }
+
+
 
 
 }
